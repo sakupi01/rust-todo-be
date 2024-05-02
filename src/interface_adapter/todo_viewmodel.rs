2bd8858b5dda0308921864acba0d526f6393ea01
saku-1101 <sakupi1101@outlook.jp>
2024-05-02 16:59:40 +0900

    add todo presenter

--- src/interface_adapter/todo_viewmodel.rs
+++ src/interface_adapter/todo_viewmodel.rs
@@ -1,3 +1,10 @@
+use std::option;
+pub struct ResultTodoViewModel {
+    pub error:option::Option<String>,
+    pub success:option::Option<String>,
+    pub error_message:option::Option<String>,
+}
+
 pub struct TodoViewModel {
     pub id: String,
     pub created_at: String,
@@ -5,4 +12,11 @@ pub struct TodoViewModel {
     pub title: String,
     pub content: String,
     pub user_id: String,
+}
+
+pub struct ResultGetTodoViewModel {
+    pub error:option::Option<String>,
+    pub success:option::Option<String>,
+    pub error_message:option::Option<String>,
+    pub todos:option::Option<Vec<TodoViewModel>>,
 }
---
+++ src/presenter/todo.rs
@@ -0,0 +1,118 @@
+use crate::{domain::todo::Todo, interface_adapter::todo_viewmodel::{ResultTodoViewModel, ResultGetTodoViewModel}};
+
+fn create(res: Result<(), String>) -> ResultTodoViewModel {
+    match res {
+        Ok(_) => ResultTodoViewModel {
+            success: Some("Success".to_string()),
+            error: None,
+            error_message: None,
+        },
+        Err(e) => ResultTodoViewModel {
+            success: None,
+            error: Some(e.to_string()),
+            error_message: None,
+        },
+    }
+}
+fn update_title(res: Result<(), String>) -> ResultTodoViewModel {
+    match res {
+        Ok(_) => ResultTodoViewModel {
+            success: Some("Success".to_string()),
+            error: None,
+            error_message: None,
+        },
+        Err(e) => ResultTodoViewModel {
+            success: None,
+            error: Some(e.to_string()),
+            error_message: None,
+        },
+    }
+}
+fn update_content(res: Result<(), String>) -> ResultTodoViewModel {
+    match res {
+        Ok(_) => ResultTodoViewModel {
+            success: Some("Success".to_string()),
+            error: None,
+            error_message: None,
+        },
+        Err(e) => ResultTodoViewModel {
+            success: None,
+            error: Some(e.to_string()),
+            error_message: None,
+        },
+    }
+}
+fn delete(res: Result<(), String>) -> ResultTodoViewModel {
+    match res {
+        Ok(_) => ResultTodoViewModel {
+            success: Some("Success".to_string()),
+            error: None,
+            error_message: None,
+        },
+        Err(e) => ResultTodoViewModel {
+            success: None,
+            error: Some(e.to_string()),
+            error_message: None,
+        },
+    }
+}
+
+fn get_all(res: Result<Vec<Todo>, String>) -> ResultGetTodoViewModel {
+    match res {
+        Ok(todos) => {
+            let todos = todos
+                .iter()
+                .map(|todo| crate::interface_adapter::todo_viewmodel::TodoViewModel {
+                    id: todo.id.clone(),
+                    created_at: todo.created_at.to_string(),
+                    updated_at: todo.updated_at.to_string(),
+                    title: todo.title.clone(),
+                    content: todo.content.clone(),
+                    user_id: todo.user_id.clone(),
+                })
+                .collect();
+            ResultGetTodoViewModel {
+                success: None,
+                error: None,
+                error_message: None,
+                todos: Some(todos),
+            }
+        }
+        Err(e) => ResultGetTodoViewModel {
+            success: None,
+            error: Some(e.to_string()),
+            error_message: None,
+            todos: None,
+        },
+    }
+}
+
+fn get_by_user_id(res: Result<Vec<Todo>, String>) -> ResultGetTodoViewModel {
+    match res {
+        Ok(todos) => {
+            let todos = todos
+                .iter()
+                .map(|todo| crate::interface_adapter::todo_viewmodel::TodoViewModel {
+                    id: todo.id.clone(),
+                    created_at: todo.created_at.to_string(),
+                    updated_at: todo.updated_at.to_string(),
+                    title: todo.title.clone(),
+                    content: todo.content.clone(),
+                    user_id: todo.user_id.clone(),
+                })
+                .collect();
+            ResultGetTodoViewModel {
+                success: None,
+                error: None,
+                error_message: None,
+                todos: Some(todos),
+            }
+        }
+        Err(e) => ResultGetTodoViewModel {
+            success: None,
+            error: Some(e.to_string()),
+            error_message: None,
+            todos: None,
+        },
+    }
+}
