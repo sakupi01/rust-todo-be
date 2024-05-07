1bd2ed7e62b21992e70075e2b854f2e21aea5671
laft2 <44256206+laft2@users.noreply.github.com>
2024-05-07 11:20:22 +0900

    add framework actix and hello_world of it

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod domain;
 pub mod interface_adapter;
 pub mod usecase;
+pub mod framework;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+// fn main() {
+//     println!("Hello, world!");
+
+// }
+mod framework;
+
+use actix_web::{web, App, HttpServer};
+use crate::framework::api_server;
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(api_server::hello)
+            .service(api_server::echo)
+            .route("/hey", web::get().to(api_server::manual_hello))
+    })
+    .bind(("127.0.0.1", 8081))?
+    .run()
+    .await
 }
